efbc93ac8d5b31c4480e84ca0d29632dd883a44d
ThorGroth <grothmichael02@gmail.com>
2025-05-19 11:41:08 +0200

    added flavours

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+flavours = [
+    {"id": 1, "name": "schokolade", "type": "milch", "price per serving": 1.5},
+    {"id": 2, "name": "vanille", "type": "milch", "price per serving": 1.5},
+    {"id": 3, "name": "zitrone", "type": "frucht", "price per serving": 1.3}
+]
+
+@app.route("/")
+def welkome():
+    return "Wilkommen auf der Homepage unserer Eisdiele"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
